cb8f22cb404284fb1f811e9c9ba1ac239af83555
Gardcs <Dmugards@gmail.com>
2023-03-16 13:33:20 +0100

    test fetching all data from the stasjon table

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hei")
+import sqlite3 as s
+
+con = s.connect("Jernbanenett.db")
+
+cursor = con.cursor()
+
+cursor.execute("SELECT * FROM Stasjon")
+
+rows = cursor.fetchall()
+print("All rows in the table stasjon:")
+print(rows)
+
+con.close()

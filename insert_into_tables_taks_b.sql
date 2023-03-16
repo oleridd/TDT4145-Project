8540b01cd5196043a2c3972607df40936c5bbb6f
Jonas Nordstrøm <jonas.nordst@gmail.com>
2023-03-16 13:41:39 +0100

    Insertion for task b

---
+++ insert_into_tables_taks_b.sql
@@ -0,0 +1,20 @@
+INSERT INTO SitteVogn (vognID, nRad, nSeterPerKupe)
+VALUES 
+(1, 3, 4),
+(2, 3, 4),
+(3, 3, 4);
+(4, 3, 4);
+
+INSERT INTO SoveVogn (vognID, nKupe, nSengPerKupe)
+VALUES
+(5, 4, 2);
+
+INSERT INTO VognTable (vognID)
+VALUES
+(1),
+(2),
+(3),
+(4),
+(5);
+
+

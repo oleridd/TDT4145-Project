b10619ab7303a5ef04d1f94f8079d65ca5308ab8
Jonas Nordstrøm <jonas.nordst@gmail.com>
2023-03-26 20:02:10 +0200

    Made second function

--- Python/hent_togruter.py
+++ Python/hent_togruter.py
@@ -24,6 +24,31 @@ def hent_togruter(stasjonID: int, ukedag: str) -> str:
 
     return np.array(cursor.fetchall()).flatten()
 
+def hent_togruter2(stasjonID: int, ukedag: str) -> str:
+    """
+    Gitt en stasjonID og en ukedag, henter alle togruter som stopper
+    på den stasjonen den gitte ukedagen.
+    
+    Argumenter:
+        stasjonID (int)
+        ukedag (string)
+    Returnerer:
+        Streng med togrutene som stopper på stasjonen
+    """
+    with sql.connect('Jernbanenett.db') as con:
+        cursor = con.cursor()
+        cursor.execute("""
+            SELECT Banestrekning.navn, startstasjon.navn, tf.avgang, endestasjon.navn, tf.ankomst
+            FROM TogruteForekomst AS tf INNER JOIN StoppPaa AS sp ON (tf.togruteforekomstID = sp.togruteforekomstID)
+            NATURAL JOIN Banestrekning
+            INNER JOIN Stasjon AS startstasjon ON (tf.startStasjonID = startstasjon.stasjonID)
+            INNER JOIN Stasjon AS endestasjon  ON (tf.endeStasjonID  = endestasjon.stasjonID)
+            WHERE tf.ukedag = (:ukedag) AND sp.stasjonID = (:stasjonID)
+        """,
+        {'ukedag': ukedag, 'stasjonID': stasjonID}
+        )
+
+    return np.array(cursor.fetchall())
 
 def hent_generell_togruteforekomst_info(togruteforekomstID: int) -> str:
     """
@@ -35,14 +60,13 @@ def hent_generell_togruteforekomst_info(togruteforekomstID: int) -> str:
         cursor.execute("""
             SELECT Banestrekning.navn, startstasjon.navn, avgang, endestasjon.navn, ankomst
             FROM Togruteforekomst NATURAL JOIN Togrute NATURAL JOIN Banestrekning
-                                  INNER JOIN Stasjon AS startstasjon ON (Togruteforekomst.startStasjonID = startstasjon.stasjonID)
-                                  INNER JOIN Stasjon AS endestasjon  ON (Togruteforekomst.endeStasjonID  = endestasjon.stasjonID)
+                INNER JOIN Stasjon AS startstasjon ON (Togruteforekomst.startStasjonID = startstasjon.stasjonID)
+                INNER JOIN Stasjon AS endestasjon  ON (Togruteforekomst.endeStasjonID  = endestasjon.stasjonID)
             WHERE togruteforekomstID = (:togruteforekomstID)
         """,
         {'togruteforekomstID': togruteforekomstID}
         )
         data = np.array(cursor.fetchall()).flatten()
-    
     return "{} | Avgang fra {}: {} | Ankomst til {}: {}".format(*data)
 
 

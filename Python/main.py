341a6d7eb820c8bdb48ea8d14658b36c888278b5
olerid <olerid02@gmail.com>
2023-03-26 20:45:05 +0200

    smol changes

--- Python/main.py
+++ Python/main.py
@@ -1,19 +1,8 @@
-import sqlite3 as sql
-from test import *
-from get_orders import get_all_tickets_for_person
-import sql_util
+from test import test_UI
 
 
 def main():
-    # test_hent_togruter()
-    # test_registrer_kunde()
-    # test_kjop_billett()
-    # test_get_train_routes_at_date()
     test_UI()
-    # test_get_train_routes_at_date()
-    # test_get_orders()
-    # sql_util.reset_database()
 
 if __name__ == "__main__":
-    
     main()
